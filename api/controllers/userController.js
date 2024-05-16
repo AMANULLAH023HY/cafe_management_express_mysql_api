c2d9da40d68c21acbb311b530d05053d91db70f1
AMANULLAH023HY <amana170829@gmail.com>
2024-05-16 16:38:19 +0530

    login route with validation

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
-const { query } = require("express");
+const JWT = require("jsonwebtoken");
 const db = require("../config/db");
 
+// user registration controller
 const signupController = (req, res) => {
   try {
     const user = req.body;
@@ -45,4 +46,52 @@ const signupController = (req, res) => {
   }
 };
 
-module.exports = { signupController };
+// user login controller
+
+const loginController = (req, res) => {
+  try {
+    const user = req.body;
+    let query = "SELECT email,password,role,status FROM user WHERE email = ?";
+
+    db.query(query, [user.email], (err, result) => {
+      if (err) {
+        res.status(500).json({
+          message: "Something went wrong!",
+          err: err.message,
+        });
+      } else {
+        if (result.length <= 0 || result[0].password != user.password) {
+          res.status(401).json({
+            message: "Incorrect user or password",
+          });
+        } else if (result[0].status == "false") {
+          res.status(401).json({
+            message: "Wait for admin approval!",
+          });
+          //   Generate JWT TOKEN
+        } else if (result[0].password == user.password) {
+          const response = { email: result[0].email, role: result[0].role };
+          const accessToken = JWT.sign(response, process.env.ACCESS_TOKEN, {
+            expiresIn: "8h",
+          });
+          res.status(200).json({
+            message: "Admin login successfully!",
+            user: result,
+            token: accessToken,
+          });
+        } else {
+          res.status(400).json({
+            message: "Something went wrong. Please try again!",
+          });
+        }
+      }
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Internal server Error",
+      error: error.message,
+    });
+  }
+};
+
+module.exports = { signupController, loginController };
--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,9 +1,12 @@
 const express = require('express');
-const { signupController } = require('../controllers/userController');
+const { signupController, loginController } = require('../controllers/userController');
 
 const router = express.Router();
 
 // New user create route
 router.post('/signup',signupController);
+
+// user login route
+router.post('/login',loginController);
 
 module.exports = router;
